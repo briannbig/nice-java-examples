81b043a1083c86b57c594ee1bbddfd39698e906a
Brian Barasa <briannbig@gmail.com>
2024-03-16 19:05:42 +0300

    Creating threads using Threads API: 5th technique - using anonymous implementation of runnable interface

--- threading/src/Main.java
+++ threading/src/Main.java
@@ -15,9 +15,27 @@ public class Main {
 //        new TensCounterTask(1.7).start();
 
         // Creating threads using Threads API: 3rd technique
-        new TwentiesCounterTask(1.2);
+//        new TwentiesCounterTask(1.2);
         // Creating threads using Threads API: 4th technique
-        new Thread( new  ThirtiesCounterTask(1.4)).start();
+//        new Thread( new  ThirtiesCounterTask(1.4)).start();
+
+        // Creating threads using Threads API: 5th technique
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous implementation of Runnable interface ");
+                for (int i = 1; i <= 10; i++) {
+                    try {
+                        Thread.sleep((long) (1.6 * 1000));
+                        System.out.println("Anonymous counter is at " + i * 40);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                System.out.println("Anonymous counter task completed");
+            }
+        }).start();
 
         Thread.sleep(5000);
         System.out.println("Main thread ends....");
